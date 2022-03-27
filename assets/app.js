0ff9433b36b8cbdde85801b62c1a68c57dd2a9de
Ashu <ashuprasad29516@gmail.com>
2022-03-27 19:33:10 +0530

    Initial commit

---
+++ assets/app.js
@@ -0,0 +1,44 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+
+function PageTransitions(){
+    // Button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += 'active-btn';
+        })
+    }
+
+    // Sctions Active 
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+
+            // Hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+
+            // Make the current section active
+            const element = document.getElementById(id);
+            console.log(id);
+            console.log(element);
+            element.classList.add('active');
+        }
+    })
+
+    // Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',() =>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+}
+
+// Call the main function
+PageTransitions();
