48b502d06082e90b0df2b7757c43858781d9ce0b
Ashu <ashuprasad29516@gmail.com>
2022-03-31 10:55:07 +0530

    Remediated light theme issue on page load

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,11 @@ const allSections = document.querySelector('.main-content');
 
 
 function PageTransitions(){
+    // Set the class name of the main section
+    if ('undefined' != localStorage["theme"]){
+        allSections.className = localStorage["theme"];
+    }
+
     // Button click active class
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
@@ -21,20 +26,30 @@ function PageTransitions(){
 
             // Hide other sections
             sections.forEach((section)=>{
-                section.classList.remove('active')
+                section.classList.remove('active');
             })
 
-            // Make the current section active
-            const element = document.getElementById(id);
-            element.classList.add('active');
+            try{
+                // Make the current section active
+                const element = document.getElementById(id);
+                element.classList.add('active');
+            }
+            catch (e){
+                console.log("Active element not found on the page");
+                console.log(e);
+            }
         }
     })
 
     // Toggle theme
     const themeBtn = document.querySelector('.theme-btn');
     themeBtn.addEventListener('click',() =>{
+        // Toggle the class name
         let element = document.body;
-        element.classList.toggle('light-mode')
+        element.classList.toggle('light-mode');
+
+        // Update the local storage variable
+        localStorage["theme"] = document.body.classList;
     })
 }
 
